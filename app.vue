43936497048514a4989ec2c330a9979a3444b724
r-herrera <russel.herrera@cambridge.org>
2022-09-09 19:00:07 +0800

    Name Cards

--- app.vue
+++ app.vue
@@ -76,7 +76,12 @@
       </div>
       <button class="primary" @click="computeSelectedNames">Find Names</button>
     </div>
-    {{ selectedNames }}
+    <div class="card-container">
+      <div v-for="name in selectedNames" :key="name" class="card">
+        <h4>{{ name }}</h4>
+        <p>x</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -168,6 +173,30 @@ h1 {
   cursor: pointer;
 }
 
+.card-container{
+  display: flex;
+  margin-top: 3rem;
+  flex-wrap: wrap;
+}
+
+.card{
+  background-color: rgb(27, 60, 138);
+  width: 28%;
+  color: white;
+  border-radius: 1rem;
+  padding: 0.1rem;
+  margin-right: 0.5rem;
+  margin-bottom: 1rem; 
+  position: relative;
+}
+
+.card p{
+  position: absolute; 
+  top: -27%; 
+  left: 92.5%;
+  cursor: pointer;
+  color: rgba(255, 255, 255, 0.178);
+}
 .option-active {
   background-color: rgb(249, 87, 89);
   color: white;
